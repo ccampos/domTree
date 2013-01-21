048733434b703a238743a41af5c8811ee26a1b86
Claudio Campos <claudio@claudiocampos.com>
2013-01-21 17:55:17 -0500

    add d3 bar chart data and yAxis labels

--- js/svg.js
+++ js/svg.js
@@ -1,14 +1,13 @@
 define(function() {
 	var initialize = function() {
 		// initialize and set svg and svg attributes
-		var svg = d3.select('svg');
-		var svgWidth = 600;
+		var svg = d3.select('svg.circles');
 		var svgHeight = 250;
-
-		svg.attr('height', svgHeight);
-		svg.attr('width', svgWidth);
+		var svgWidth = 550;
 
 		// set svg width and height
+		svg.attr('height', svgHeight);
+		svg.attr('width', svgWidth);
 
 		// initialize circles
 		var circles = svg.selectAll('circle');
@@ -47,11 +46,85 @@ define(function() {
 	};
 
 	var displaySVGtree = function(data) {
+		var svg = d3.select('svg.branch');
+		var svgHeight = 200;
+		var svgWidth = 800;
 
+		svg.attr('height', svgHeight);
+		svg.attr('width', svgWidth);
+	};
+
+	var displayBarChart = function() {
+		var svg = d3.select('svg.barChart');
+
+		// create data array
+		var data = [
+			{ year: 2009, books: 54 },
+			{ year: 2010, books: 43 },
+			{ year: 2011, books: 35 },
+			{ year: 2012, books: 75 },
+			{ year: 2013, books: 51 }
+		];
+
+		// set bar chart width
+		var barWidth = 40;
+
+		// set svg height and width
+		var svgHeight = 200;
+		var svgWidth = (barWidth + 10) * data.length;
+
+		svg.attr('height', svgHeight);
+		svg.attr('width', svgWidth);
+
+		// set x and y scales
+		var xScale = d3.scale.linear()
+		.domain([0, data.length]) // data minimum and maximum
+		.range([0, svgWidth]); // the pixels to map to, e.g., the width of the diagram
+
+		var yScale = d3.scale.linear()
+		.domain([0, d3.max(data, function(datum) { return datum.books; })])
+		.rangeRound([0, svgHeight]);
+
+		// add bars
+		svg.selectAll('rect')
+			.data(data)
+			.enter()
+			.append('svg:rect')
+			.attr('x', function(datum, index) { return xScale(index); })
+			.attr('y', function(datum) { return svgHeight - yScale(datum.books); })
+			.attr('height', function(datum) { return yScale(datum.books); })
+			.attr('width', barWidth)
+			.attr('fill', '#2d578b');
+
+		// add text
+		svg.selectAll('text')
+			.data(data)
+			.enter()
+			.append('svg:text')
+			.attr('x', function(datum, index) { return xScale(index) + + barWidth; })
+			.attr('y', function(datum) { return svgHeight - yScale(datum.books); })
+			.attr('dx', -barWidth/2)
+			.attr('dy', '1.2em')
+			.attr('text-anchor', 'middle')
+			.text(function(datum) { return datum.books; })
+			.attr('fill', 'white');
+
+		svg.selectAll('text.yAxis')
+			.data(data)
+			.enter().append('svg:text')
+			.attr('x', function(datum, index) { return xScale(index) + barWidth; })
+			.attr('y', svgHeight)
+			.attr('dx', -barWidth/2)
+			.attr('text-anchor', 'middle')
+			.attr('style', 'font-size: 12; font-family: Helvetica, sans-serif')
+			.text(function(datum) { return datum.year; })
+			.attr('transform', 'translate(0, 18)')
+			.attr('class', 'yAxis');
 	};
 
 	return {
 		initialize: initialize,
-		displaySVGtree: displaySVGtree
+		displaySVGtree: displaySVGtree,
+		displayBarChart: displayBarChart
 	};
 });
